49a96b4dd029fe12f0c3b04fbd5bd437ab0d6af6
Noahhalv <noahhalv08@gmail.com>
2026-05-11 15:08:16 +0200

    did stuff

---
+++ tournament-site/src/Components/Navbar.jsx
@@ -0,0 +1,22 @@
+import './Navbar.css'
+
+export default function Navbar() {
+
+    return (
+        <>
+            <nav className="navbar">
+                <section className="left">
+
+                </section>
+
+                <section className="center">
+
+                </section>
+
+                <section className="right">
+                    
+                </section>
+            </nav>
+        </>
+    )
+}
